c9f50474fba2519e8a2073c13c869f4a4bbac2c3
sebasmadrizz <madrizsebas71@gmail.com>
2025-07-02 10:48:30 -0600

    estructura

---
+++ Peliculas.API/API/Program.cs
@@ -0,0 +1,40 @@
+using Abstracciones.Interfaces.DA;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Interfaces.Servicios;
+using DA;
+using DA.Repositorio;
+using Flujo;
+using Reglas;
+using Servicios;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpClient();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRepositorioDapper,RepositorioDapper >();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
